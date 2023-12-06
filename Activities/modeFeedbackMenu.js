2a5054c79f4f7c5c7212cc8ba14ed3ddf8d79c5b
KiyotakkkkA <KirZaha2@mail.ru>
2023-12-06 04:11:09 +0300

    update

---
+++ Activities/modeFeedbackMenu.js
@@ -0,0 +1,18 @@
+const _bot = require("../ROOT/BOT")
+const root = require("../ROOT/ROOT");
+
+const {error_messages, spec_symbols} = require("../BotConfig/config_messages")
+
+function feedbackActivity(text, ChatId, msg) {
+    if (text == "Обратная связь"){
+        _bot.BotMsg(ChatId, spec_symbols['SB_write'] + " Оставьте вашу жалобу/предложение, при возможности прикрепите фотографию (ответьте на это сообщение)")
+    }
+    if (msg.reply_to_message){
+        if (msg.reply_to_message.text == spec_symbols['SB_write'] + " Оставьте вашу жалобу/предложение, при возможности прикрепите фотографию (ответьте на это сообщение)"){
+            _bot.BotMsg(root.__ROOT_ID__, "Сообщение от: <b><u>" + msg.chat.first_name + " " + msg.chat.last_name + "</u></b> - @" + msg.chat.username)
+            _bot.bot.forwardMessage(root.__ROOT_ID__, ChatId, msg.message_id)
+        }
+    }
+}
+
+module.exports = {feedbackActivity}
